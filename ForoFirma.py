3b014ee4bcd1c937205bab499bf68c0d8df4ed9f
RafaRojasH <94881844+RafaRojasH@users.noreply.github.com>
2022-08-19 21:02:21 -0500

    Update ForoFirma.py

--- ForoFirma.py
+++ ForoFirma.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import urllib
+import base64
+import fitz
 
+def read_pdf_with_fitz(file):
+ 	with fitz.open(file) as doc:
+ 		text = ""
+ 		for page in doc:
+ 			text += page.getText()
+ 		return text 
+
+uploaded_file = st.file_uploader("Choose a file", type=['pdf'], accept_multiple_files=True)
+
+if uploaded_file is not None:
+    st.subheader('llll')
+    read_pdf_with_fitz(uploaded_file)
